7d7693c358780d8eb458e9d48f31962ee64d744c
Jacob Borg <jacob.d.borg@gmail.com>
2020-04-29 16:49:36 -0700

    Biggest index update yet!
    20+ sites added!
    1 new Python config script!
    And more!

---
+++ InputConfig.py
@@ -0,0 +1,5 @@
+url = ""
+h1 = ""
+seo = ""
+description = ""
+print('<li><a href="' + url + '" rel="noreferrer">' + url + '<p name="h1" hidden>' + h1 + '</p><p name="tags" hidden>' + seo + '</p><p class="pc_s">' + description + '</p></a></li>')
